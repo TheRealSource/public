fe01f7f10773755aa13abafa2a0eed2785a08a9e
Hellsing <matrix11@gmx.net>
2014-03-30 17:11:50 +0200

    Final fix for LazyUpdater...

--- common/SourceLib.lua
+++ common/SourceLib.lua
@@ -3,7 +3,7 @@
 local autoUpdate = true
 local silentUpdate = false
 
-local version = 0.5
+local version = 0.6
 
 --[[
 
@@ -126,7 +126,7 @@ function LazyUpdater:CheckUpdate()
             if self.FILE_VERSION < self.SERVER_VERSION then
                 self.printMessage("New version available: " .. self.SERVER_VERSION)
                 self.printMessage("Updating, please don't press F9")
-                DownloadFile(self.UPDATE_URL, self.UPDATE_FILE_PATH, function () self.printMessage("Successfully updated. (v" .. self.FILE_VERSION .. " -> v" .. self.SERVER_VERSION .. "), press F9 twice to load the updated version.") end)
+                DelayAction(function () DownloadFile(self.UPDATE_URL, self.UPDATE_FILE_PATH, function () self.printMessage("Successfully updated, please reload!") end) end, 2)
             else
                 self.printMessage("You've got the latest version: v" .. self.SERVER_VERSION)
             end
